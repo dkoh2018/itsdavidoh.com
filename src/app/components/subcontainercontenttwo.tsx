28aa7098b6cfce7c53104f15587339aec1cdd8fd
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-07-12 02:18:45 -0500

    styling ya dg

---
+++ src/app/components/subcontainercontenttwo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./styles/subcontainercontenttwo.module.css";
+import PPImage from "./ppimage";
+
+const SubContainerContentTwo = () => {
+  return <div className={styles.profileSection}></div>;
+};
+
+export default SubContainerContentTwo;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import styles from "./components/styles/homepage.module.css";
 import Container from "./components/container";
 import SubContainer from "./components/subcontainer";
 import SubContainerContentOne from "./components/subcontainercontentone";
+import SubContainerContentTwo from "./components/subcontainercontenttwo";
 
 const HomePage = () => {
   return (
     <div className={styles.page}>
       <Container>
         <SubContainerContentOne />
-        {/* <SubContainer></SubContainer> */}
+        <SubContainerContentTwo />
       </Container>
     </div>
   );
