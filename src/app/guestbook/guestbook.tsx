e6b1ceab4921a5727dbd41361ce66bda0d098b0a
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-08-01 00:44:48 -0400

    got rid of postgres since code base got messed up. Redo backened for guestbook. restart

--- src/app/guestbook/guestbook.tsx
+++ src/app/guestbook/guestbook.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { Section } from "../_components/section";
-import githubIcon from "../img/githublogo.svg";
 import Link from "next/link";
 
 export const metadata = {
@@ -11,13 +9,13 @@ export const metadata = {
 export default function GuestBook() {
   return (
     <Section title="Guestbook">
-      <div className="flex-grow flex items-center justify-start my-10">
-        <button className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 mb-5">
-          <Image
+      <div className="flex-grow flex items-center justify-center my-10">
+        <button className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 mb-8">
+          <img
             alt="GitHub logo"
-            src={githubIcon}
-            width={20}
-            height={20}
+            src="/src/app/img/githublogo.svg"
+            width="20"
+            height="20"
           />
           <div className="ml-3">Sign in with GitHub</div>
         </button>
