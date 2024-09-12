525d164801998e473aa9043c33ed8b814d6011b0
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-09-12 16:59:42 -0500

    automatic push for website updates/changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,17 @@ export default function Projects() {
         <Link href="/" className="text-miami-blue underline mb-8">
           back to main
         </Link>
-        <iframe src="https://youtu.be/ixGGdUoFKbE" allowFullScreen className="w-full h-96 mb-8" />
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/ixGGdUoFKbE?si=UN4VOnBy5RkrASOR"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="w-full h-96 mb-8"
+        />
         <h2 className="text-xl font-bold mb-4">google cal on your terminal</h2>
         <p className="mb-4">
           this project combines the gcsa package, google calendar api, and openai to create an
